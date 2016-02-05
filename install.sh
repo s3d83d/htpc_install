e212896a2012ca05b575b34f636c87dd4fb66e7b
Matt Eary <matt@Matts-MBP.localdomain>
2016-02-04 20:06:39 -0600

    added modules

--- install.sh
+++ install.sh
@@ -11,11 +11,24 @@
 # 
 
 ## Variables
-os_ver=`lsb_release -a | grep -i release | awk '{print $2}'`
+if [ -f /etc/os-release ]; then
+	os_fam=`cat /etc/os-release | grep -ie "\<name\>" | awk -F"\"" '{print $2}'
+	os_ver=`lsb_release -a | grep -i release | awk '{print $2}'`
+fi
+current_dir="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
+## Functions
+function _usage(){
+	echo -e "\nsudo ${current_dir}/install.sh [ complete ] [ htpc | plex | nzbdrone | couchpotato | nzbget | apache ]\n"
+}
 ## Fact Checking
-if [ "${os_ver}" != '14.04']; then
-	read -p "This script was tested on Ubuntu 14.04. Your version [${os_ver}] has not been verified to work with this script. Do you wish to continue? (y/n)" y
+if [ `whoami` != 'root']; then
+	echo -e "\nYou must run with root privileges (i.e. sudo)"
+	_usage
+fi
+
+if [ "${os_fam,,}" != 'ubuntu' ] && [ "${os_ver}" != '14.04']; then
+	read -p "This script was tested on Ubuntu 14.04. Your version [${os_fam} ${os_ver}] has not been verified to work with this script. Do you wish to continue? (y/n)" y
 	case $yn in
 		[Yy]* ) break;;
 		[Nn]* ) 
