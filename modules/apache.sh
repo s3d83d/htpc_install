c2aaa70d943dbdf32318611572397204a82e2fb6
s3d83d <matt@earys.com>
2016-02-06 16:26:00 -0600

    finished code overhaul. getting ready to test

--- modules/apache.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Installing Apache..."
-myip=$(ifconfig | grep -e "\<inet\>" | grep -v 127.0.0.1 | awk '{print $2}' | awk -F: '{print $2}')
-
-sudo apt-get install apache2 -y
-
-sudo a2enmod proxy proxy_ajp proxy_http rewrite deflate headers proxy_balancer proxy_connect proxy_html
-
